c674be643249c3c964dbe2268084847c54eb15e2
Ivona Mircheva <Ivona.Mircheva@calastone.com>
2023-08-03 17:16:36 +0100

    Cry

--- Bombe/Program.cs
+++ Bombe/Program.cs
@@ -4,37 +4,40 @@ using System.IO;
 static void Main(string[] args)
 {
     string dayOfWeek = "MONDAY";
+    string input = "";
     if (args == null || args.Length < 2)
     {
         Console.WriteLine("Day shall be Monday");
     }
     else
     {
-        var input = args[0];
+        input = args[0];
         dayOfWeek = args[1];
     }
 
-    //var fullOptions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
+    char[] fullAlphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
 
-    //dayOfWeekInString = thursday(grab today or arg)
+    string repeatedDayOFWeek = "";
+    do
+    {
+        repeatedDayOFWeek = repeatedDayOFWeek + dayOfWeek;
+    }
+    while (repeatedDayOFWeek.Length <= input.Length);
 
-    //do
-    //    current = current + dayOfWeekInString
-    //while currnwt.lenght <= input.length
+    var finalCut = repeatedDayOFWeek.Substring(0, input.Length);
 
-    //var finalCut = current.substring(0, input.length)
+    var result = new List<char>();
 
-    //var result = new List<char>();
-    //for (var i = 0; i++; i < input.lenght)
-    //{
-    //    forecach(var letterFromDay in finalCut)
-    //    {
-    //        var indexCurrentInInputFound = fullOptions.Find(letterFromDay.ToString()).IndexOf();
-    //        var indexOfOldLetter = fullOptions.Find(input[i] - indexCurrentInInputFound).IndexOf();
-    //        var newLetter = fullOptions[indexOfOldLetter];
-    //        result.Add(newLetter);
-    //    }
-    //}
+    for (var i = 0; i < input.Length; i++)
+    {
+        finalCut.ToCharArray()
+        {
+            var indexCurrentInInputFound = fullAlphabet.Find(letterFromDay.ToString()).IndexOf();
+            var indexOfOldLetter = fullAlphabet.Find(input[i] - indexCurrentInInputFound).IndexOf();
+            var newLetter = fullAlphabet[indexOfOldLetter];
+            result.Add(newLetter);
+        }
+    }
 
-//return result;
+    //return result;
 }
